9c5d5b295f80355da0e8ce4dd76ec664f88e6018
David Castelletti <castellettid@gmail.com>
2021-01-29 10:23:27 +0100

    Setup iniziale del progetto

---
+++ src/main/java/com/davioooh/ciaomondo/CiaoMondoApplication.java
@@ -0,0 +1,13 @@
+package com.davioooh.ciaomondo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiaoMondoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiaoMondoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davioooh/ciaomondo/CiaoMondoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davioooh.ciaomondo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiaoMondoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
