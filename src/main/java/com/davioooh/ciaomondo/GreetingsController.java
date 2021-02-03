55fcae8711b48acd6f14f970a7a703ebda3f4c5a
David Castelletti <castellettid@gmail.com>
2021-02-03 15:35:32 +0100

    Esempi request mapping

--- src/main/java/com/davioooh/ciaomondo/GreetingsController.java
+++ src/main/java/com/davioooh/ciaomondo/GreetingsController.java
@@ -1,14 +1,26 @@
 package com.davioooh.ciaomondo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class GreetingsController {
 
-  @RequestMapping("/")
+  @GetMapping
   public String greetings() {
     return "hello-world";
+  }
+
+  @GetMapping(params = "req-param")
+  public String greetingsWithParam() {
+    return "hello-world-params";
+  }
+
+  @GetMapping(headers = "req-header=hValue")
+  public String greetingsWithHeader() {
+    return "hello-world-headers";
   }
 
 }
