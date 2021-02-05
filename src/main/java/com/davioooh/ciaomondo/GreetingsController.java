4ce98ae516b9623350cbfd040a827883782a5b99
David Castelletti <castellettid@gmail.com>
2021-02-05 11:34:42 +0100

    Esempi di utilizzo dei model attribute

--- src/main/java/com/davioooh/ciaomondo/GreetingsController.java
+++ src/main/java/com/davioooh/ciaomondo/GreetingsController.java
@@ -1,16 +1,33 @@
 package com.davioooh.ciaomondo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalTime;
 
 @Controller
 @RequestMapping("/")
 public class GreetingsController {
 
   @GetMapping
-  public String greetings() {
+  public String greetings(Model model) {
+    String greetingsMessage = greetingsByTime(LocalTime.now());
+    model.addAttribute("message", greetingsMessage);
     return "hello-world";
+  }
+
+  public static String greetingsByTime(LocalTime time) {
+    String greetings;
+    if (time.isBefore(LocalTime.NOON)) {
+      greetings = "Buongiorno";
+    } else if (time.isBefore(LocalTime.of(18, 0))) {
+      greetings = "Buon pomeriggio";
+    } else {
+      greetings = "Buonasera";
+    }
+    return greetings + " Mondo!";
   }
 
   @GetMapping(params = "req-param")
